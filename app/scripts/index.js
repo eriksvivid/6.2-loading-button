3f5700d9f5757a29a8f51939927ade77117add11
Erik <sanchezerik78@yahoo.com>
2016-04-23 18:00:05 -0400

    working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,18 +1,20 @@
 var $ = require('jquery');
+var Backbone = require('backbone');
+var handlebars = require('handlebars');
+var _ = require('underscore');
 
-var models = require('./models/collections.js');
-var pokemons = new models.PokemonCollection();
+var Pokemon = require('./models/collections');
+var pokemons = new Pokemon.PokemonCollection();
 
-var pokemonView = {
-  clear: function(){
-    $('.container').html('');
-  },
-  render: function(pokemon){
-    $('.container').append('<div>' + pokemon.get("name") + ': ' + pokemon.get("types") + '</div>');
-  }
-}
-
-pokemons.fetch().done(function(){
-  pokemons.each(function(pokemonStat){
-    pokemonView.render(pokemonStat);
+$('.search').on('click', function(){
+  $('.btn').html('Loading..');
+  this.disabled = true;
+  pokemons.fetch().done(function(event){
+    $('.btn').html('Submit');
+    document.getElementsByClassName('.btn').disabled = false;
+    event.results.map(function(pokemon){
+      // console.log(pokemon.name);
+      $('.container').append('<li>' + pokemon.name + '</li>');
+    });
+  });
 });
--- app/scripts/models/collections.js
+++ app/scripts/models/collections.js
@@ -2,18 +2,21 @@ var Backbone = require('backbone');
 
 
 var Pokemon = Backbone.Model.extend({
-  urlRoot: 'http://pokeapi.co/api/v2/'
+
 });
 
 var PokemonCollection = Backbone.Collection.extend({
   model: Pokemon,
-  url: 'http://pokeapi.co/api/v2/',
+  url: 'http://pokeapi.co/api/v2/pokemon/',
   parse: function(data){
     return data.results;
   }
 });
 
+// var pokemonCollect = new PokemonCollection()
+
+
 module.exports = {
   'Pokemon': Pokemon,
   'PokemonCollection': PokemonCollection
-}
+};
